5b03caaf11b3a783321773680878b93c63e551c3
umaibal <umai.balendra@shopify.com>
2018-07-07 18:03:19 -0400

    chap 6 depot app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+#
+Product.create!(title: 'Seven Mobile Apps',
+  description:
+    %{<p>
+      <em>Native Apps</em>
+      Answer the question, can we build this app for all devices?
+      Lets make the coolest app in the existence of apps,
+      so that in the app store, our app will gain the highest
+      reviews and have the most downloads.
+      </p>},
+  image_url: '7apps.jpg',
+  price: 26.00)
+  # 
