5cb2220b357e05864ec3810d341ccf60f839dcb6
umaibal <umai.balendra@shopify.com>
2018-07-15 16:59:56 -0400

    complete E2-Handling errors

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,12 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+
+  private
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_index_url, notice: 'Invalid cart'
+  end
 
   # GET /carts
   # GET /carts.json
