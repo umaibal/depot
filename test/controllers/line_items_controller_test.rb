01da84ea0eaac8e94aa47acbf2f33ebfd0cdcb3d
umaibal <umai.balendra@shopify.com>
2018-07-15 17:31:33 -0400

    complete E3-Finishing the Cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -22,9 +22,9 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
 
-    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'h2', 'Your Cart'
     assert_select 'li', 'Home'
-    # assert_select 'li', '1 \00D7 Programming Ruby 1.9'
+    # assert_select 'td', "Programming Ruby 1.9"
   end
 
   test "should show line_item" do
