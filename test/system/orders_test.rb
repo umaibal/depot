260cf86570baa51610606a5c441f162e211f5373
umaibal <umai.balendra@shopify.com>
2018-07-16 16:22:17 -0400

    complete H2-Testing JS Functionality

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,49 +1,21 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
-  setup do
-    @order = orders(:one)
-  end
+  test "check routing number" do
+    visit store_index_url 
 
-  test "visiting the index" do
-    visit orders_url
-    assert_selector "h1", text: "Orders"
-  end
+    first('.catalog li').click_on 'Add to Cart'
 
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
+    click_on 'Checkout'
 
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay Type", with: @order.pay_type
-    click_on "Create Order"
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
 
-    assert_text "Order was successfully created"
-    click_on "Back"
-  end
+    assert_no_selector "#order_routing_number"
 
-  test "updating a Order" do
-    visit orders_url
-    click_on "Edit", match: :first
+    select 'Check', from: 'pay_type'
 
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay Type", with: @order.pay_type
-    click_on "Update Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Order" do
-    visit orders_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Order was successfully destroyed"
+    assert_selector "#order_routing_number"
   end
 end
