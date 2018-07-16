c706bc418538bf027920e0814e085850b3fa63ca
umaibal <umai.balendra@shopify.com>
2018-07-16 13:01:47 -0400

    complete H1-Add Fields Dynamically

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,20 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay_type_params 
+    if order_params[:pay_type] == "Credit Card"
+      params.require(:order).permit(:credit_card_number,
+         :expiration_date) 
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number,
+         :account_number) 
+    elsif order_params[:pay_type] == "Purchase Order"
+      params.require(:order).permit(:po_number) 
+    else
+      {}
+    end
+  end
+
   # GET /orders
   # GET /orders.json
   def index
